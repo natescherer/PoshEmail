2e0dc6004b7784124fa12a8d3cb141bf26edc538
Nate Scherer <376408+natescherer@users.noreply.github.com>
2018-10-31 15:17:16 -0400

    Fix test typo casued by find and replace

--- test/PoshEmail.Tests.ps1
+++ test/PoshEmail.Tests.ps1
@@ -616,7 +616,7 @@ InModuleScope $ModuleName {
             "                                <table border=`"0`" cellpadding=`"0`" cellspacing=`"0`" style=`"border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: auto;`">$Eol" +
             "                                  <tbody>$Eol" +
             "                                    <tr>$Eol" +
-            "                                      <td style=`"font-family: sans-serif; font-size: 14px; vertical-align: top; background-color: #3498db; border-radius: $ProccessStartSleeppx; text-align: center;`"> <a href=`"https://testbuttonlink.com`" target=`"_blank`" style=`"display: inline-block; color: #ffffff; background-color: #3498db; border: solid 1px #3498db; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-transform: capitalize; border-color: #3498db;`">Test ButtonText</a> </td>$Eol" +
+            "                                      <td style=`"font-family: sans-serif; font-size: 14px; vertical-align: top; background-color: #3498db; border-radius: 5px; text-align: center;`"> <a href=`"https://testbuttonlink.com`" target=`"_blank`" style=`"display: inline-block; color: #ffffff; background-color: #3498db; border: solid 1px #3498db; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-transform: capitalize; border-color: #3498db;`">Test ButtonText</a> </td>$Eol" +
             "                                    </tr>$Eol" +
             "                                  </tbody>$Eol" +
             "                                </table>$Eol" +
