498546ddf8041fd13d18fc588612380a031baecf
Nate Scherer <376408+natescherer@users.noreply.github.com>
2021-03-30 15:23:20 -0400

    Fix path case

--- src/PoshEmail.psm1
+++ src/PoshEmail.psm1
@@ -2,13 +2,13 @@
 # in the format 'This-IsFunctionName.ps1'. Put functions that are only used internally by this module in the 'Private'
 # directory in the same name format.
 
-$PrivateFiles = Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -Recurse
+$PrivateFiles = Get-ChildItem -Path $PSScriptRoot\private\*.ps1 -Recurse
 foreach ($PrivateFile in $PrivateFiles) {
     . $PrivateFile.FullName
 }
 
 $FunctionsToExport = @()
-$PublicFiles = Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -Recurse
+$PublicFiles = Get-ChildItem -Path $PSScriptRoot\public\*.ps1 -Recurse
 foreach ($PublicFile in $PublicFiles) {
     . $PublicFile.FullName
     $FunctionsToExport += [io.path]::GetFileNameWithoutExtension($PublicFile.FullName)
